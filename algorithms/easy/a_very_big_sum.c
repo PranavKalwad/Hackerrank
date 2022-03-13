89236d7d3951ce8a78b23cf2b815c48d87fa5a7f
Pranav Kalwad <70849056+PranavKalwad@users.noreply.github.com>
2022-03-13 22:36:05 +0530

    Delete algorithms/easy directory

--- algorithms/easy/a_very_big_sum.c
+++
@@ -1,9 +0,0 @@
-long aVeryBigSum(int ar_count, long* ar) {
-    
-    long int sum=0;
-    for(int i=0;i<ar_count;i++)
-    {
-        sum+=ar[i];
-    }
-    return sum;
-}
--- algorithms/easy/staircase.py
+++
@@ -1,3 +0,0 @@
-def staircase(n):
-    for x in range(1, n+1):
-        print(" " * (n - x) + "#" * x )
